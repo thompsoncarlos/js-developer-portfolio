c9e7110331c76b5ec9854d146bfd0c6134c832e7
Carlos Thompson <carlosethompson@gmail.com>
2024-04-18 00:24:16 +0100

    update dinamization with portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,16 +19,16 @@ function updateProfileInfo(profileData) {
   github.innerText = profileData.github;
 }
 
-function updateProfileSoftSkills(profileData) {
+function updateSoftSkills(profileData) {
   const softSkills = document.getElementById("profile.skills.softSkills");
   softSkills.innerHTML = profileData.skills.softSkills
     .map((skill) => `<li>${skill}</li>`)
     .join("");
 }
 
-function updateProfileHardSkills(profileData) {
-  const hardSkills = document.getElementById("profile.skills.hardSkills");
-  hardSkills.innerHTML = profileData.skills.hardSkills
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById("profile.skills.hardSkills");
+    hardSkills.innerHTML = profileData.skills.hardSkills
     .map(
       (skill) =>
         ` <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`
@@ -36,9 +36,29 @@ function updateProfileHardSkills(profileData) {
     .join("");
 }
 
+function updateLanguages(profileData) {
+  const languages = document.getElementById("profile.languages");
+  languages.innerHTML = profileData.languages
+    .map((language) => `<li>${language}</li>`)
+    .join("");
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById("profile.portfolio");
+  portfolio.innerHTML = profileData.portfolio
+    .map((project) => ` 
+    <li>
+        <h3 ${project.github ? 'class="github"' : ''} >${project.name}</h3>
+        <a href="${project.url}" target="_blank">${project.url}</a>
+  </li>`)
+    .join("");
+}
+
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
-  updateProfileSoftSkills(profileData);
-  updateProfileHardSkills(profileData);
+  updateSoftSkills(profileData);
+  updateHardSkills(profileData);
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
 })();
