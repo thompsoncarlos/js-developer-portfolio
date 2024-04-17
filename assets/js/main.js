aa521005d19420eed085a5342a46af89411920c1
Carlos Thompson <carlosethompson@gmail.com>
2024-04-17 23:28:34 +0100

    fix: add images and update profile.json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,44 @@
+function updateProfileInfo(profileData) {
+  const photo = document.getElementById("profile.photo");
+  photo.src = profileData.photo;
+  photo.alt = profileData.name;
+
+  const name = document.getElementById("profile.name");
+  name.innerText = profileData.name;
+
+  const job = document.getElementById("profile.job");
+  job.innerText = profileData.job;
+
+  const location = document.getElementById("profile.location");
+  location.innerText = profileData.location;
+
+  const linkedin = document.getElementById("profile.linkedin");
+  linkedin.innerText = profileData.linkedin;
+
+  const github = document.getElementById("profile.github");
+  github.innerText = profileData.github;
+}
+
+function updateProfileSoftSkills(profileData) {
+  const softSkills = document.getElementById("profile.skills.softSkills");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
+}
+
+function updateProfileHardSkills(profileData) {
+  const hardSkills = document.getElementById("profile.skills.hardSkills");
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(
+      (skill) =>
+        ` <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`
+    )
+    .join("");
+}
 
 (async () => {
-    const profileData = await fetchProfileData();
-    console.log(profileData);
-})()
+  const profileData = await fetchProfileData();
+  updateProfileInfo(profileData);
+  updateProfileSoftSkills(profileData);
+  updateProfileHardSkills(profileData);
+})();
